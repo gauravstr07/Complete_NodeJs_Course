e35eed7d3e1bdd96bf57eecac757f143926af769
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-04 19:42:38 +0530

    Read file data from FS

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const dirPath = path.join(__dirname, 'files');
+const dirPath = path.join(__dirname, "files");
 
-for(let i = 0; i < 5; i++){
-    fs.writeFileSync(dirPath + "/hello"+i+".txt", 'A simple text file')
-}
+// for (let i = 0; i < 5; i++) {
+//   fs.writeFileSync(dirPath + "/hello" + i + ".txt", "A simple text file");
+// }
 
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((item) => {
+    console.log(item);
+  });
+});
